d8542c6ea6c33d32fa0d4b69ac15c7a135a2018a
jcalcausal <jcalcausal@gmail.com>
2025-02-24 19:26:14 +0100

    Hasta la pg 55

---
+++ Examples/src/main/scala/main.scala
@@ -0,0 +1,33 @@
+import scala.collection.mutable
+@main
+def main(): Unit =
+  /*
+  El código de abajo es incorrecto, ya que por defecto un Map es inmutable.
+  Si queremos poder modificarlo hay que declararlo como mutable
+  val capital = Map("US" -> "Washington", "France" -> "Paris")
+  capital += ("Japan" -> "Tokyo")
+  println(capital("France"))
+   */
+  // Versión correcta: referencia inmutable (val), estructura mutable
+  val capital = mutable.Map("US" -> "Washington", "France" -> "Paris")
+  capital += ("Japan" -> "Tokyo")
+  println(capital("France"))
+  /*
+  A continuación se muestra la diferencia entre una clase en Java y una en Scala:
+  1) En Java:
+  class MyClass { // this is Java
+    private int index;
+    private String name;
+    public MyClass(int index, String name) {
+    this.index = index;
+    this.name = name;
+    }
+  }
+  2) En Scala:
+  class MyClass(index: Int, name: String)
+
+  Por defecto, el compilador de Scala produce una clase con dos variables de instancia
+  privadas y un constructor por defecto que toma como argumento ambas.
+  La única diferencia es que las variables de instancia generadas en Scala serán final
+  
+  */
