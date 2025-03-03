c6cfdcde3b7f74914f509227adef331b9506f578
jcalcausal <jcalcausal@gmail.com>
2025-03-03 18:58:27 +0100

    Ejercicio 3, MCD y mcm

--- Ejercicios/Tema-1-y-2/src/main/scala/Ejercicios.scala
+++ Ejercicios/Tema-1-y-2/src/main/scala/Ejercicios.scala
@@ -1,5 +1,5 @@
 /*
-Implementa una función que compruebe si una cadena dada es un palíndromo
+1) Implementa una función que compruebe si una cadena dada es un palíndromo
 (ignorando mayúsculas y minúsculas y espacios).
 Nota: utilizar las funciones de la librería para la clase String.
 */
@@ -8,6 +8,9 @@ def palindromo(palabra: String): Boolean =
   val palabra_rev_sin_sp: String = palabra.reverse.filter(c => c != ' ')
   palabra_sin_sp == palabra_rev_sin_sp
 
+/*
+2) Crea un programa que imprima los primeros n números primos.
+*/
 def primerosPrimos(n:Int): Unit =
   var i = 2
   var c = 0
@@ -20,3 +23,16 @@ def primerosPrimos(n:Int): Unit =
       c += 1
     num += 1
     i = 2
+
+/*
+3) Implementa un programa que calcule el máximo común divisor (MCD) y el mínimo común múltiplo (MCM)
+de dos números. Nota: el tipo devuelto por la función es una tupla de dos componentes.
+*/
+
+def maximoMinimo(n1:Int,n2:Int): (Int,Int) =
+  def euclides(a: Int, b: Int): Int =
+    if (b == 0) a
+    else euclides(b, a % b)
+  val MCD = euclides(n1, n2)
+  val mcm = Math.abs(n1 * n2) / MCD
+  (MCD, mcm)
