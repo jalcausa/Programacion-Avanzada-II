69c2836f23a0c29a29b9a8320a0723b8da3b558b
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-05-09 13:44:12 +0200

    Ejercicio 4

--- Practicas/Practica-6.1/src/main/scala/concurrencia/Ejercicio4.scala
+++ Practicas/Practica-6.1/src/main/scala/concurrencia/Ejercicio4.scala
@@ -4,34 +4,51 @@ import java.util.concurrent.*
 import scala.util.Random
 
 class Coche(C: Int) extends Thread {
-  // CS-pasajero1: si el coche está lleno, un pasajero no puede subir al coche hasta que haya terminado
+  // CS1-pasajero1: si el coche está lleno, un pasajero no puede subir al coche hasta que haya terminado
   // el viaje y se hayan bajado los pasajeros de la vuelta actual
-  // CS-pasajero2: un pasajero que está en el coche no puede bajarse hasta que haya terminado el viaje
+  // CS2-pasajero: un pasajero que está en el coche no puede bajarse hasta que haya terminado el viaje
   // CS-coche: el coche espera a que se hayan subido C pasajeros para dar una vuelta
   private var numPas = 0
-  // ...
+  private val mutex = new Semaphore(1)
+  private val puertaEntrada = new Semaphore(1)
+  private val puertaSalida = new Semaphore(0)
+  private val estaLleno = new Semaphore(0)
 
   def nuevoPaseo(id: Int) = {
     // el pasajero id quiere dar un paseo en la montaña rusa
-    // ...
+    puertaEntrada.acquire()
+    mutex.acquire() // No haría falta ya que puertaEntrada hace como mutex en este caso
+    numPas += 1
     log(s"El pasajero $id se sube al coche. Hay $numPas pasajeros.")
-    // ...
+    if (numPas < C)  // Si no está lleno el coche dejo entrar a más pasajeros
+      puertaEntrada.release()
+
+    if (numPas == C) // Si soy el último despierto a esperaLleno para que empiece el viaje
+      estaLleno.release()
+    mutex.release()
+    puertaSalida.acquire()
+    // Cuando estoy aquí salgo del coche
+    mutex.acquire()
+    numPas -= 1
     log(s"El pasajero $id se baja del coche. Hay $numPas pasajeros.")
-    // ...
+    if (numPas > 0)
+      puertaSalida.release()
+    else
+      puertaEntrada.release()
+    mutex.release()
   }
 
   def esperaLleno = {
     // el coche espera a que se llene para dar un paseo
-    // ...
+    estaLleno.acquire()
     log(s"        Coche lleno!!! empieza el viaje....")
-    // ...
+
   }
 
   def finViaje = {
     // el coche indica que se ha terminado el viaje
-    // ...
     log(s"        Fin del viaje... :-(")
-    // ...
+    puertaSalida.release()
   }
 
   override def run = {
