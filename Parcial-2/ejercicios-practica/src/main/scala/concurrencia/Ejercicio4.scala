92b7fce6c18b6be4a043b9c69f747806efeecdeb
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-05-13 21:09:13 +0200

    Ej 4

--- Parcial-2/ejercicios-practica/src/main/scala/concurrencia/Ejercicio4.scala
+++ Parcial-2/ejercicios-practica/src/main/scala/concurrencia/Ejercicio4.scala
@@ -9,29 +9,44 @@ class Coche(C: Int) extends Thread {
   // CS-pasajero2: un pasajero que está en el coche no puede bajarse hasta que haya terminado el viaje
   // CS-coche: el coche espera a que se hayan subido C pasajeros para dar una vuelta
   private var numPas = 0
-  // ...
+  private val mutex = new Semaphore(1)
+  private val lleno = new Semaphore(0)
+  private val puertaEntrada = new Semaphore(1)
+  private val puertaSalida =  new Semaphore(0)
 
   def nuevoPaseo(id: Int) = {
     // el pasajero id quiere dar un paseo en la montaña rusa
-    // ...
+    puertaEntrada.acquire()
+    mutex.acquire()
+    numPas += 1
     log(s"El pasajero $id se sube al coche. Hay $numPas pasajeros.")
-    // ...
+    if (numPas == C)
+      lleno.release()
+    else
+      puertaEntrada.release()
+    mutex.release()
+    puertaSalida.acquire()
+    mutex.acquire()
+    numPas -= 1
     log(s"El pasajero $id se baja del coche. Hay $numPas pasajeros.")
-    // ...
+    mutex.release()
+    if (numPas > 0)
+      puertaSalida.release()
+    if (numPas == 0)
+      puertaEntrada.release()
+    mutex.release()
   }
 
   def esperaLleno = {
     // el coche espera a que se llene para dar un paseo
-    // ...
+    lleno.acquire()
     log(s"        Coche lleno!!! empieza el viaje....")
-    // ...
   }
 
   def finViaje = {
     // el coche indica que se ha terminado el viaje
-    // ...
     log(s"        Fin del viaje... :-(")
-    // ...
+    puertaSalida.release()
   }
 
   override def run = {
