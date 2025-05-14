52c73406bf84d74f1a9edbe5a2d293c1911f013c
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-05-14 14:58:54 +0200

    Ejercicio 7

--- Parcial-2/ejercicios-practica/src/main/scala/concurrencia/Ejercicio7.scala
+++ Parcial-2/ejercicios-practica/src/main/scala/concurrencia/Ejercicio7.scala
@@ -8,20 +8,35 @@ class Nido(B: Int) {
   // CS-papá/mamá: no puede dejar un bichito en el plato si está lleno
 
   private var plato = 0
-  // ...
+  private val mutex = new Semaphore(1)
+
+  private val hayComida = new Semaphore(0)
+  private val hayEspacio = new Semaphore(1)
 
   def cojoBichito(i: Int): Unit = {
     // el bebé i coge un bichito del plato
-    // ...
+    hayComida.acquire()
+    mutex.acquire()
+    plato -=1
     log(s"Bebé $i coge un bichito. Quedan $plato bichitos")
-    // ...
+    if (plato > 0)
+      hayComida.release()
+    if (plato == B - 1)
+      hayEspacio.release()
+    mutex.release()
   }
 
   def pongoBichito(i: Int): Unit = {
     // el papá/la mamá pone un bichito en el plato (0=papá, 1=mamá)
-    // ...
+    hayEspacio.acquire()
+    mutex.acquire()
+    plato += 1
     log(s"Papá $i pone un bichito. Quedan $plato bichitos")
-    // ...
+    if (plato == 1)
+      hayComida.release()
+    if (plato < B)
+      hayEspacio.release()
+    mutex.release()
   }
 }
 
