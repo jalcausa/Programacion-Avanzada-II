3caf9ea802a64045530af77f4b8f5095a73ad3a1
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-05-14 14:31:12 +0200

    Ejercicio 5

--- Parcial-2/ejercicios-practica/src/main/scala/concurrencia/Ejercicio5.scala
+++ Parcial-2/ejercicios-practica/src/main/scala/concurrencia/Ejercicio5.scala
@@ -8,26 +8,36 @@ object gestorAgua {
   // CS-Hid2: Un hidrógeno debe esperar a los otros dos átomos para formar la molécula
   // CS-Ox1: El oxígeno que quiere formar una molécula espera si ya hay un oxígeno
   // CS-Ox2: El oxígeno debe esperar a los otros dos átomos para formar la molécula
-
+  private var nHidrogeno = 0
+  private val mutex = new Semaphore(1)
+  private val puertaOx = new Semaphore(1)
+  private val puertaH = new Semaphore(1)
+  private val formaMolecula = new Semaphore(0)
 
   def oxigeno(id: Int) = {
     // el oxígeno id quiere formar una molécula
-    // ...
+    puertaOx.acquire()
     log(s"Oxígeno $id quiere formar una molécula")
-    // ...
-    // log(s"      Molécula formada!!!")
-    // ...
-    // log(s"Sale oxígeno $id: numO: $numO---molecula=${molecula.availablePermits()}")
-    // ...
+    formaMolecula.acquire()
+    mutex.acquire()
+    log(s"      Molécula formada!!!")
+    puertaOx.release()
+    nHidrogeno = 0
+    puertaH.release()
+    mutex.release()
   }
 
   def hidrogeno(id: Int) = {
     // el hidrógeno id quiere formar una molécula
-    // ...
+    puertaH.acquire()
+    mutex.acquire()
     log(s"Hidrógeno $id quiere formar una molécula")
-    // ...
-    // log(s"      Molécula formada!!!")
-    // ...
+    nHidrogeno += 1
+    if (nHidrogeno == 2)
+      formaMolecula.release()
+    else
+      puertaH.release()
+    mutex.release()
   }
 }
 object Ejercicio5 {
