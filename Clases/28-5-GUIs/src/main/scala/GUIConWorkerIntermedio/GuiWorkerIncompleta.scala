3e813e8e11cffd643c46fbffe0fca29ebb0a72be
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-06-04 14:55:53 +0200

    Clase 4 de junio

---
+++ Clases/28-5-GUIs/src/main/scala/GUIConWorkerIntermedio/GuiWorkerIncompleta.scala
@@ -0,0 +1,163 @@
+package GUIConWorkerIntermedio
+
+import java.awt.event.{ActionEvent, ActionListener}
+import java.awt.{BorderLayout, FlowLayout, GridLayout}
+import java.beans.{PropertyChangeEvent, PropertyChangeListener}
+import java.util
+import javax.swing.*
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.CancellationException
+
+class Worker(n:Int, panel:Panel) extends SwingWorker[java.util.List[Int],Unit]{
+
+  def esPrimo(n: Int): Boolean = {
+    def loop(div: Int): Boolean = {
+      if (div * div > n) true
+      else if (n % div == 0) false
+      else loop(div + 1)
+    }
+
+    if (n == 1 || n == 2) true
+    else loop(2)
+  }
+
+  def listaPrimos(n: Int): java.util.List[Int] =
+    val lista = new util.ArrayList[Int]()
+
+    def loop(i: Int, pprimo: Int): java.util.List[Int] = {
+      if (i == n || this.isCancelled) lista
+      else {
+        if (esPrimo(pprimo))
+          lista.add(pprimo)
+          loop(i + 1, pprimo + 1)
+        else
+          loop(i, pprimo + 1)
+      }
+    }
+
+    loop(0, 1)
+  override def doInBackground(): util.List[Int] = {
+    listaPrimos(n)
+  }
+
+  override def done(): Unit = {
+    try{
+      panel.listaPrimos(get())
+      panel.nuevoMensaje("Tarea finalizada")
+    } catch{
+      case e:CancellationException => panel.nuevoMensaje("Tarea cancelada")
+    }
+
+  }
+}
+class Controlador(panel:Panel) extends ActionListener {
+
+  private var worker: Worker = null
+
+  override def actionPerformed(e: ActionEvent): Unit = {
+    if (e.getActionCommand.equals("SI")) {
+      panel.nuevoMensaje("Sí pulsado")
+    } else if (e.getActionCommand.equals("NO")) {
+      panel.nuevoMensaje("No pulsado")
+    } else if (e.getActionCommand.equals("CANCELAR")) {
+      if (worker!=null) worker.cancel(true)
+    } else
+    //e.getActionCommand.equals("PRIMOS")
+      try {
+        val n = panel.numero()
+        panel.limpiarArea()
+        // panel.nuevoMensaje("Canculando primos....")
+        //   val lista = listaPrimos(n)
+        //  panel.listaPrimos(lista)
+        // panel.nuevoMensaje("Tarea finalizada")
+        worker = new Worker(n, panel)
+        worker.execute()
+      } catch {
+        case e: NumberFormatException => panel.nuevoMensaje("Número incorrecto")
+      }
+  }
+}
+
+
+
+
+
+
+  class Panel extends JPanel{
+  private val panelNorte = new JPanel()
+  private val etiq = new JLabel("es verdad?")
+  private val bsi = new JButton("Sí")
+  private val bno = new JButton("No")
+  private val etiqPrimos = new JLabel("Primos?:")
+  private val nprimos = new JTextField(5)
+  private val mensaje = new JLabel("GUI creada")
+  private val area = new JTextArea(30,30)
+  private val cancelar = new JButton("Cancelar")
+
+  panelNorte.add(etiq)
+  panelNorte.add(bsi)
+  panelNorte.add(bno)
+  panelNorte.add(etiqPrimos)
+  panelNorte.add(nprimos)
+  panelNorte.add(cancelar)
+
+  private val scrol = new JScrollPane(area)
+  this.setLayout(new BorderLayout())
+  this.add(panelNorte,BorderLayout.NORTH)
+
+  this.add(mensaje,BorderLayout.SOUTH)
+  this.add(scrol,BorderLayout.CENTER)
+
+  def controlador(ctr:ActionListener)={
+    bsi.setActionCommand("SI")
+    bno.setActionCommand("NO")
+    bsi.addActionListener(ctr)
+    bno.addActionListener(ctr)
+    nprimos.setActionCommand("PRIMOS")
+    nprimos.addActionListener(ctr)
+    cancelar.setActionCommand("CANCELAR")
+    cancelar.addActionListener(ctr)
+  }
+
+  def nuevoMensaje(str:String) = {
+    mensaje.setText(str)
+  }
+
+  def numero():Int = {
+    Integer.parseInt(nprimos.getText)
+  }
+
+  def listaPrimos(lista:java.util.List[Int]) ={
+    for (i<-0 until lista.size())
+      area.append(s"${lista.get(i)} ")
+      if ((i+1)%10 == 0) area.append("\n")
+  }
+  def limpiarArea()=
+    area.setText("")
+}
+
+
+object EjemploGui {
+
+  def crearGUI(ventana:JFrame)={
+    val panel = new Panel
+    val ctr = new Controlador(panel)
+    panel.controlador(ctr)
+    ventana.setContentPane(panel)
+    ventana.pack()
+    ventana.setVisible(true)
+    ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+  }
+
+  def main(args:Array[String]): Unit = {
+    SwingUtilities.invokeLater(
+      new Runnable:
+        override def run(): Unit =
+          val ventana = new JFrame("Ejemplo sencillo")
+          crearGUI(ventana)
+    )
+
+
+  }
+
+}
--- Clases/28-5-GUIs/src/main/scala/GUISimple/EjemploGUI.scala
+++ Clases/28-5-GUIs/src/main/scala/GUISimple/EjemploGUI.scala
@@ -2,27 +2,86 @@ package GUISimple
 
 import java.awt.event.{ActionEvent, ActionListener}
 import java.awt.{BorderLayout, FlowLayout, GridLayout}
-import javax.swing.{JButton, JFrame, JLabel, JPanel, JScrollPane, JTextArea, JTextField, WindowConstants}
+import java.util
+import javax.swing.{JButton, JFrame, JLabel, JPanel, JScrollPane, JTextArea, JTextField, SwingUtilities, SwingWorker, WindowConstants}
 
-class Controlador extends ActionListener {
+/*
+Podemos hacer que todos lo relacionado con calcular los primos lo haga otra hebra a parte de tipo worker
+para no tener que hacer eso desde la hebra de controlador
+class Worker(n:Int, panel: Panel) extends SwingWorker[java.util.List[Int], Unit] = {
+
+}
+*/
+
+class Controlador (panel: Panel) extends ActionListener {
+
+  def esPrimo(n: Int): Boolean = {
+    def loop(div: Int): Boolean = {
+      if (div*div > n) true
+      else if (n % div == 0) false
+      else loop(div + 1)
+    }
+    if (n == 1 || n == 2) true
+    else loop(2)
+  }
+
+  def listaPrimos(n: Int): java.util.List[Int] = {
+    val lista = new util.ArrayList[Int]()
+
+    def loop(i: Int, primo: Int): java.util.List[Int] = {
+      if (i == n) lista
+      else {
+        if (esPrimo(primo)) {
+          lista.add(primo)
+          loop(i + 1, primo + 1)
+        } else
+          loop(i, primo + 1)
+      }
+    }
+
+    loop(0, 1)
+  }
 
   override def actionPerformed(e: ActionEvent): Unit = {
     if (e.getActionCommand.equals("SI")) {
-      // Codigo SI
-    } else {
-      // Codigo NO
+      panel.nuevoMensaje("Sí pulsado")
+    } else if (e.getActionCommand.equals("NO")) {
+      panel.nuevoMensaje("No pulsado")
+    } else { // e.getActionCommand.equals("PRIMOS")
+      try {
+        val n = panel.numero()
+        val lista = listaPrimos(n)
+        panel.listaPrimos(lista)
+      } catch {
+        case e:NumberFormatException => panel.nuevoMensaje("Número incorrecto")
+      }
     }
   }
-
 }
 
 class Panel extends JPanel {
+  this.setLayout(new BorderLayout())
+  private val panelNorte = new JPanel()
   private val etiq = new JLabel("es verdad?")
   private val bsi = new JButton("Sí")
   private val bno = new JButton("No")
-  this.add(etiq)
-  this.add(bsi)
-  this.add(bno)
+  private val etiqPrimos = new JLabel("Primos?:")
+  private val nprimos = new JTextField(5)
+  private val area = new JTextArea(30, 30)
+  // private val scrol = new JScrollPane(area) // Arreglar esto
+
+  private val mensaje = new JLabel("No se ha pulsado ningún botón")
+
+  panelNorte.add(etiq)
+  panelNorte.add(bsi)
+  panelNorte.add(bno)
+  panelNorte.add(etiqPrimos)
+  panelNorte.add(nprimos)
+
+
+  this.add(panelNorte, BorderLayout.NORTH)
+  this.add(area, BorderLayout.CENTER)
+  this.add(mensaje, BorderLayout.SOUTH)
 
   // Hay que vincular el controlador con el elemento visual para que
   // cuando ocurra el evento el controlador ejecute la función que lo maneja
@@ -31,44 +90,35 @@ class Panel extends JPanel {
     bsi.setActionCommand("SI")
     bno.addActionListener(ctr);
     bno.setActionCommand("NO")
+
+    nprimos.addActionListener(ctr);
+
+  }
+
+  def nuevoMensaje(str: String) = {
+    mensaje.setText(str)
+  }
+
+  def numero(): Int = {
+    Integer.parseInt(nprimos.getText)
+  }
+
+  def listaPrimos(lista: java.util.List[Int]) = {
+    for (i<- 0 until lista.size()) {
+      area.append(s"${lista.get((i))} ")
+      if ((i+1)%10 == 0) area.append("\n")
+    }
+  }
+
+  def limpiarArea() = {
+    area.setText("")
   }
 }
 
-class Panel2 extends JPanel {
-  this.setLayout(new BorderLayout())
-
-  private val etiqTam = new JLabel("tamaño?:")
-  private val tam = new JTextField(4)
-  private val botonNorte = new JButton("Norte")
-  private val panelNorte = new JPanel()
-  panelNorte.add(botonNorte); panelNorte.add(etiqTam); panelNorte.add(tam)
-
-  private val botonSur = new JButton("Sur")
-
-  private val areaCentro = new JTextArea(30, 20)
-  private val scrollArea = new JScrollPane(areaCentro)
-
-  private val botonEste = new JButton("Este")
-  private val botonOeste1 = new JButton("Oeste 1")
-  private val botonOeste2 = new JButton("Oeste 2")
-
-  this.add(panelNorte, BorderLayout.NORTH)
-  this.add(botonSur, BorderLayout.SOUTH)
-  this.add(botonEste, BorderLayout.EAST)
-  this.add(scrollArea, BorderLayout.CENTER)
-
-  private val panelOeste = new JPanel()
-  panelOeste.setLayout(new GridLayout(2, 1))
-  panelOeste.add(botonOeste1)
-  panelOeste.add(botonOeste2)
-  this.add(panelOeste, BorderLayout.WEST)
-}
-
 object EjemploGUI {
-  def main(args: Array[String]): Unit = {
-    val ventana = JFrame(("Un ejemplo"))
+  def crearGUI(ventana: JFrame) = {
     val panel = new Panel
-    val ctr = new Controlador
+    val ctr = new Controlador(panel)
     panel.controlador(ctr)
 
     ventana.setContentPane(panel)
@@ -76,5 +126,15 @@ object EjemploGUI {
     ventana.pack()
     ventana.setVisible(true)
     ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+  }
+
+  def main(args: Array[String]): Unit = {
+    SwingUtilities.invokeLater(
+      new Runnable {
+        override def run(): Unit =
+          val ventana = JFrame(("Un ejemplo"))
+          crearGUI(ventana)
+      }
+    )
   }
 }
